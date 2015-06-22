8090531494b7e7bf1cccb961030ca95f89aca701
Daniel Kostro <kostro.d@gmail.com>
2015-06-22 15:43:08 +0200

    Version option can be auto

--- src/index.js
+++ src/index.js
@@ -9,22 +9,11 @@ var superagent = require('superagent');
 var component = React.createClass({
 
     render: function () {
-        if (!this.state.version && this.props.version === 'auto' && this.props.viewURL) {
-            this.getVersion();
-            return <div/>;
-        }
-
-
-        var version;
-        if (this.props.viewURL)
-            version = this.state.version || this.props.version || 'latest';
-        else
-            version = 'latest';
-
-        var cdn = this.props.cdn || '//www.lactame.com/visualizer';
+        var cdn = this.props.cdn ? this.props.cdn.replace(/\/$/, '') : '//www.lactame.com/visualizer';
         var viewURL = this.props.viewURL || '';
         var dataURL = this.props.dataURL || '';
         var config = this.props.config || '';
+        var version = this.props.version || 'latest';
         var width = this.props.width || '100%';
         var height = this.props.height || '100%';
 
@@ -38,23 +27,16 @@ var component = React.createClass({
         var query = ['viewURL', viewURL, 'dataURL', dataURL, 'config', config];
         query = query.map(function (v, i) {
             if (i % 2 === 1)
-                return query[i-1] + '=' + encodeURIComponent(query[i]);
-        }).filter(function(v) {
+                return query[i - 1] + '=' + encodeURIComponent(query[i]);
+        }).filter(function (v) {
             return v !== undefined
         });
         query = query.join('&');
 
-        return (
-            <iframe src={cdn + '/' + version + '/index.html?' + query} style={style}/>
-        );
-    },
-
-    getVersion() {
-        setTimeout(() => {
-            this.setState({
-                version: 'v2.20.1'
-            });
-        }, 1000)
+        if (this.props.version !== 'auto') { // Force version to be loaded
+            return <iframe src={cdn + '/' + version + '/index.html?' + query} style={style}/>;
+        }
+        return <iframe src={cdn + '/load.html?' + query} style={style}/>;
     },
 
     getInitialState() {
--- test/app.js
+++ test/app.js
@@ -5,7 +5,7 @@ import Visualizer from '../src/index';
 
 let element = React.createElement(Visualizer, {
     //cdn: '//www.lactame.com/visualizer',
-    //version: 'v2.20.1', // auto
+    version: 'auto',
     viewURL: 'http://couch.cheminfo.org/cheminfo/65f84b002399eb79ec0f8bf145fa63cc/view.json'
     //dataURL: '',
     //config: ''
