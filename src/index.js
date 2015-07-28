2d4df0bd2c8d470de344e1480aa6d11ff06dfdb2
Daniel Kostro <kostro.d@gmail.com>
2015-07-28 11:02:16 +0200

    Use blob url as visualizer index page

--- src/index.js
+++ src/index.js
@@ -4,10 +4,22 @@
 'use strict';
 
 var React = require('react');
+
+var page = require('./visualizer.js');
+var url;
+
 var component = React.createClass({
 
     render: function () {
-        var cdn = this.props.cdn ? this.props.cdn.replace(/\/$/, '') : '//www.lactame.com/visualizer';
+
+        var cdn = this.props.cdn ? this.props.cdn.replace(/\/$/, '') : 'https://www.lactame.com/visualizer';
+        var h = page.replace('{{ cdn }}', '"' + cdn + '"');
+        var scripts = this.props.scripts || [];
+        var scriptsStr = scripts.reduce(function (value, script) {
+            return value + `<script src="${script}"></script>\n`;
+        }, '');
+        h = h.replace('{{scripts}}', scriptsStr);
+        if(!url) url = URL.createObjectURL(new Blob([h], {type: 'text/html'}));
         var viewURL = this.props.viewURL || '';
         var dataURL = this.props.dataURL || '';
         var config = this.props.config || '';
@@ -31,10 +43,17 @@ var component = React.createClass({
         });
         query = query.join('&');
 
+
         if (this.props.version !== 'auto') { // Force version to be loaded
-            return <iframe src={cdn + '/' + version + '/index.html?' + query} style={style}/>;
+            query += '&v=' + version;
+            return (
+                <iframe src={url + '#?' + query} style={style}>
+                    <div/>
+                </iframe>
+                );
         }
-        return <iframe src={cdn + '/load.html?' + query} style={style}/>;
+        query += '&loadversion=true';
+        return <iframe src={url + '#?' + query} style={style}/>;
     },
 
     getInitialState() {
--- test/app.js
+++ test/app.js
@@ -6,7 +6,8 @@ import Visualizer from '../src/index';
 let element = React.createElement(Visualizer, {
     //cdn: '//www.lactame.com/visualizer',
     version: 'auto',
-    viewURL: 'http://couch.cheminfo.org/cheminfo/65f84b002399eb79ec0f8bf145fa63cc/view.json'
+    viewURL: 'http://couch.cheminfo.org/cheminfo/65f84b002399eb79ec0f8bf145fa63cc/view.json',
+    scripts: ['http://www.lactame.com/lib/ml/HEAD/ml.js']
     //dataURL: '',
     //config: ''
 });
