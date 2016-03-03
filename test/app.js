72bb0ea2155ec11de2e4d3bc028670a3d1ab9033
Daniel Kostro <kostro.d@gmail.com>
2016-03-03 17:07:45 +0100

    more efficient version loading

--- test/app.js
+++ test/app.js
@@ -8,7 +8,7 @@ let element = React.createElement(Visualizer, {
     //cdn: '//www.lactame.com/visualizer',
     fallbackVersion: 'HEAD',
     version: 'auto',
-    viewURL: 'http://couch.cheminfo.org/cheminfo/65f84b002399eb79ec0f8bf145fa63cc/view.json',
+    viewURL: 'http://127.0.0.1:5984/myviews/1a978de7ca7383a6854f5eb1280b8ce1/view.json',
     scripts: [
         'http://www.lactame.com/lib/ml/HEAD/ml.js',
         {url: 'http://www.lactame.com/lib/semver/4.3.1/semver.js'},
