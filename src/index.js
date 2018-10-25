20ded1411873825f831e2a589619a6424a7ac89e
Michaël Zasso <targos@protonmail.com>
2018-10-25 17:11:05 +0200

    feat: upgrade to React 16
    BREAKING CHANGE: Upgrade to React 16

--- src/index.js
+++ src/index.js
@@ -1,95 +1,100 @@
 'use strict';
 
-var React = require('react');
-var urlNode = require('url');
+const React = require('react');
 
-var page = require('./visualizer.js');
-var urls = new Map();
-var configs = new Map();
+const page = require('./visualizer.js');
+const urls = new Map();
+const configs = new Map();
 
-var component = React.createClass({
+function relativeUrl(from, to) {
+  return new URL(to, from).href;
+}
 
-    render: function () {
-        var currentHref = window.location.href;
-        var cdn = this.props.cdn ? this.props.cdn.replace(/\/$/, '') : 'https://www.lactame.com/visualizer';
-        cdn = urlNode.resolve(currentHref, cdn);
-        var fallbackVersion = this.props.fallbackVersion || 'latest';
-        var h = page.replace(/\{\{ cdn }}/g, cdn);
-        h = h.replace(/\{\{ fallbackVersion }}/g, fallbackVersion);
-        var scripts = this.props.scripts || [];
-        var scriptsStr = scripts.reduce(function (value, script) {
-            if (script.url || typeof script === 'string') {
-                var url = script.url;
-                if (url) {
-                    url = urlNode.resolve(currentHref, url);
-                }
-                return value + `<script src="${url || script}"></script>\n`;
-            } else if (script.content) {
-                return value + `<script>${script.content}</script>\n`;
-            } else {
-                throw new Error('script must have url or content');
-            }
-        }, '');
-        h = h.replace('{{scripts}}', scriptsStr);
-
-        if(!urls.has(h)) {
-            urls.set(h, URL.createObjectURL(new Blob([h], {type: 'text/html'})));
+class ReactVisualizer extends React.PureComponent {
+  render() {
+    var currentHref = window.location.href;
+    var cdn = this.props.cdn
+      ? this.props.cdn.replace(/\/$/, '')
+      : 'https://www.lactame.com/visualizer';
+    cdn = relativeUrl(currentHref, cdn);
+    var fallbackVersion = this.props.fallbackVersion || 'latest';
+    var h = page.replace(/\{\{ cdn }}/g, cdn);
+    h = h.replace(/\{\{ fallbackVersion }}/g, fallbackVersion);
+    var scripts = this.props.scripts || [];
+    var scriptsStr = scripts.reduce(function(value, script) {
+      if (script.url || typeof script === 'string') {
+        var url = script.url;
+        if (url) {
+          url = relativeUrl(currentHref, url);
         }
-        var url = urls.get(h);
-        var viewURL = this.props.viewURL || '';
-        if (viewURL) viewURL = urlNode.resolve(currentHref, viewURL);
-        var dataURL = this.props.dataURL || '';
-        if (dataURL) dataURL = urlNode.resolve(currentHref, dataURL);
-        var config = this.props.config || '';
-        var version = this.props.version || 'latest';
+        return value + `<script src="${url || script}"></script>\n`;
+      } else if (script.content) {
+        return value + `<script>${script.content}</script>\n`;
+      } else {
+        throw new Error('script must have url or content');
+      }
+    }, '');
+    h = h.replace('{{scripts}}', scriptsStr);
 
-        if(typeof config === 'object' && config !== null) {
-            if (config.header && config.header.elements) {
-                config = JSON.parse(JSON.stringify(config));
-                var els = config.header.elements;
-                for (var i = 0; i < els.length; i++) {
-                    if (els[i].url) els[i].url = urlNode.resolve(currentHref, els[i].url);
-                }
-            }
-            var configJson = JSON.stringify(config);
-            if(!configs.has(configJson))
-                configs.set(configJson, URL.createObjectURL(new Blob([configJson], {type: 'application/json'})));
-            config = configs.get(configJson);
+    if (!urls.has(h)) {
+      urls.set(h, URL.createObjectURL(new Blob([h], { type: 'text/html' })));
+    }
+    var url = urls.get(h);
+    var viewURL = this.props.viewURL || '';
+    if (viewURL) viewURL = relativeUrl(currentHref, viewURL);
+    var dataURL = this.props.dataURL || '';
+    if (dataURL) dataURL = relativeUrl(currentHref, dataURL);
+    var config = this.props.config || '';
+    var version = this.props.version || 'latest';
+
+    if (typeof config === 'object' && config !== null) {
+      if (config.header && config.header.elements) {
+        config = JSON.parse(JSON.stringify(config));
+        var els = config.header.elements;
+        for (var i = 0; i < els.length; i++) {
+          if (els[i].url) els[i].url = relativeUrl(currentHref, els[i].url);
         }
-
-        var style = this.props.style || {
-            width: '100%',
-            height: '100%',
-            position: 'absolute',
-            border: 'none'
-        };
-
-        var query = ['viewURL', viewURL, 'dataURL', dataURL, 'config', config];
-        query = query.map(function (v, i) {
-            if (i % 2 === 1)
-                return query[i - 1] + '=' + encodeURIComponent(query[i]);
-        }).filter(function (v) {
-            return v !== undefined
-        });
-        query = query.join('&');
-
-
-        if (version !== 'auto' || !viewURL) { // Force version to be loaded
-            var v = version === 'auto' ? 'latest' : version;
-            query += '&v=' + v;
-        }
-        else {
-            query += '&loadversion=true';
-        }
-        return <iframe allowFullScreen="true" src={url + '#?' + query} style={style}/>;
-    },
-
-    getInitialState() {
-        return {
-            version: null
-        }
+      }
+      var configJson = JSON.stringify(config);
+      if (!configs.has(configJson))
+        configs.set(
+          configJson,
+          URL.createObjectURL(
+            new Blob([configJson], { type: 'application/json' })
+          )
+        );
+      config = configs.get(configJson);
     }
 
-});
+    var style = this.props.style || {
+      width: '100%',
+      height: '100%',
+      position: 'absolute',
+      border: 'none'
+    };
 
-module.exports = component;
+    var query = ['viewURL', viewURL, 'dataURL', dataURL, 'config', config];
+    query = query
+      .map(function(v, i) {
+        if (i % 2 === 1)
+          return query[i - 1] + '=' + encodeURIComponent(query[i]);
+      })
+      .filter(function(v) {
+        return v !== undefined;
+      });
+    query = query.join('&');
+
+    if (version !== 'auto' || !viewURL) {
+      // Force version to be loaded
+      var v = version === 'auto' ? 'latest' : version;
+      query += '&v=' + v;
+    } else {
+      query += '&loadversion=true';
+    }
+    return (
+      <iframe allowFullScreen="true" src={url + '#?' + query} style={style} />
+    );
+  }
+}
+
+module.exports = ReactVisualizer;
