9ce02d338bf68f67cc3ad038d855dac5abb1bf1a
Michaël Zasso <targos@protonmail.com>
2024-05-28 09:53:52 +0200

    test: add demo for new component

--- test/app.js
+++ test/app.js
@@ -2,21 +2,27 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {OldReactVisualizer} from '../src';
+import {OldReactVisualizer, ReactVisualizer} from '../src';
 
-let element = React.createElement(OldReactVisualizer, {
+const oldElement = React.createElement(OldReactVisualizer, {
     //cdn: '//www.lactame.com/visualizer',
     fallbackVersion: 'HEAD',
-    version: 'auto',
-    viewURL: 'https://myviews.cheminfo.org/db/visualizer/entry/a27d59b20df929892c57461a030640a5/view.json',
     scripts: [
         'https://www.lactame.com/lib/ml/HEAD/ml.js',
         {url: 'https://www.lactame.com/lib/semver/4.3.1/semver.js'},
         {content: 'var TEST = 42;'}
-    ]
+    ],
+    version: 'auto',
+    viewURL: 'https://myviews.cheminfo.org/db/visualizer/entry/a27d59b20df929892c57461a030640a5/view.json',
     //config: "https://www.lactame.com/visualizer/HEAD/usr/config/default.json"
     //dataURL: '',
     //config: ''
 });
 
-ReactDOM.createRoot(document.getElementById('visualizer')).render(element);
+const newElement = React.createElement(ReactVisualizer, {
+    url: 'visualizer.html',
+    version: 'auto',
+    viewURL: 'https://myviews.cheminfo.org/db/visualizer/entry/a27d59b20df929892c57461a030640a5/view.json',
+});
+
+ReactDOM.createRoot(document.getElementById('visualizer')).render(window.location.search.includes('new') ? newElement : oldElement);
