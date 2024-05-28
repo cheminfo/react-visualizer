79a00ec2380c6792eb70edc749057573964bd9ee
Michaël Zasso <targos@protonmail.com>
2024-05-28 09:36:20 +0200

    feat: add ReactVisualizer component without data URL for the iframe

--- src/OldReactVisualizer.jsx
+++ src/OldReactVisualizer.jsx
@@ -3,9 +3,9 @@
 const React = require("react");
 
 const makeVisualizerPage = require("./makeVisualizerPage");
+const ReactVisualizer = require("./ReactVisualizer");
 
 const urls = new Map();
-const configs = new Map();
 
 function relativeUrl(from, to) {
   return new URL(to, from).href;
@@ -41,59 +41,16 @@ class OldReactVisualizer extends React.PureComponent {
       urls.set(page, URL.createObjectURL(new Blob([page], { type: 'text/html' })));
     }
     const url = urls.get(page);
-    let viewURL = this.props.viewURL || '';
-    if (viewURL) viewURL = relativeUrl(currentHref, viewURL);
-    let dataURL = this.props.dataURL || '';
-    if (dataURL) dataURL = relativeUrl(currentHref, dataURL);
-    let config = this.props.config || '';
-    const version = this.props.version || 'latest';
 
-    if (typeof config === 'object' && config !== null) {
-      if (config.header && config.header.elements) {
-        config = JSON.parse(JSON.stringify(config));
-        var els = config.header.elements;
-        for (var i = 0; i < els.length; i++) {
-          if (els[i].url) els[i].url = relativeUrl(currentHref, els[i].url);
-        }
-      }
-      var configJson = JSON.stringify(config);
-      if (!configs.has(configJson))
-        configs.set(
-          configJson,
-          URL.createObjectURL(
-            new Blob([configJson], { type: 'application/json' })
-          )
-        );
-      config = configs.get(configJson);
-    }
-
-    var style = this.props.style || {
-      width: '100%',
-      height: '100%',
-      position: 'absolute',
-      border: 'none'
-    };
-
-    var query = ['viewURL', viewURL, 'dataURL', dataURL, 'config', config];
-    query = query
-      .map(function(v, i) {
-        if (i % 2 === 1)
-          return query[i - 1] + '=' + encodeURIComponent(query[i]);
-      })
-      .filter(function(v) {
-        return v !== undefined;
-      });
-    query = query.join('&');
-
-    if (version !== 'auto' || !viewURL) {
-      // Force version to be loaded
-      var v = version === 'auto' ? 'latest' : version;
-      query += '&v=' + v;
-    } else {
-      query += '&loadversion=true';
-    }
     return (
-      <iframe allowFullScreen src={url + '#?' + query} style={style} />
+      <ReactVisualizer
+        url={url}
+        viewURL={this.props.viewURL}
+        dataURL={this.props.dataURL}
+        config={this.props.config}
+        version={this.props.version}
+        style={this.props.style}
+      />
     );
   }
 }
